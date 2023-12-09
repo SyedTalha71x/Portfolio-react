761f46868afabe85f50fffaeb9296f3426fff11a
Talha <syedtalha71x@gmail.com>
2023-12-09 12:40:16 +0500

    new changes made

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -29,19 +29,19 @@ export const Navbar = (props) => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="nav_items navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a class="nav-link" href="/">Home</a>
+                                <a className="nav-link" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a class="nav-link" href="./#projects">Projects</a>
+                                <a className="nav-link" href="./#projects">Projects</a>
                             </li>
                             <li className="nav-item">
-                                <a class="nav-link" href="./#contact">Contact</a>
+                                <a className="nav-link" href="./#contact">Contact</a>
                             </li>
                             <li className="nav-item">
-                                <a class="nav-link" href="./#services">Services</a>
+                                <a className="nav-link" href="./#services">Services</a>
                             </li>
                             <li className="nav-item">
-                                <a class="nav-link" href="./#abouts">About</a>
+                                <a className="nav-link" href="./#abouts">About</a>
                             </li>
 
                         </ul>
@@ -51,13 +51,15 @@ export const Navbar = (props) => {
             </nav>
 
             <div className="home-content">
-                <h2>I'm Talha, a Professional Web Developer</h2>
-                <p>I am a passionate Full Stack Web Developer in the Software Industry. Just hire me One time. </p>
-                <div className="main-icons">
-                    <i class="fa-brands fa-linkedin" onClick={onlinkdin}></i>
-                    <i class="fa-brands fa-github" onClick={ongit}></i>
-                    <i class="fa-brands fa-instagram" onClick={oninsta}></i>
-                    <i class="fa-brands fa-facebook" onClick={onfb}></i>
+                <div className="heading">
+                    <h2>I'm, Talha a Professional Web Developer</h2>
+                    <p>I am a passionate Web Developer in the Software industry. Just hire me one time!</p>
+                    <div className="main-icons">
+                        <i class="fa-brands fa-facebook" onClick={onfb}></i>
+                        <i class="fa-brands fa-github" onClick={ongit}></i>
+                        <i class="fa-brands fa-instagram" onClick={oninsta}></i>
+                        <i class="fa-brands fa-linkedin" onClick={onlinkdin}></i>
+                    </div>
                 </div>
             </div>
         </>
